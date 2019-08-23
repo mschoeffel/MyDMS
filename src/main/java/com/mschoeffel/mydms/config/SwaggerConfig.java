dea964eba14600628429eead44969292106ffc95
mschoeffel <michael.schoeffel@t-online.de>
2019-08-23 11:39:30 +0200

    Added Swagger description

--- src/main/java/com/mschoeffel/mydms/config/SwaggerConfig.java
+++ src/main/java/com/mschoeffel/mydms/config/SwaggerConfig.java
@@ -1,14 +1,16 @@
 package com.mschoeffel.mydms.config;
 
-import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -19,6 +21,13 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/*"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("My REST API", "Some custom description of API.", "API TOS",
+                "Terms of service", new Contact("myName", "myUrl", "myMail"), "License of API",
+                "API license URL", Collections.emptyList());
     }
 }
